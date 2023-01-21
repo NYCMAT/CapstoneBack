ffdcfd70d689119b3a866c00be7318b8a205da51
Matthew Chin <matthewchin@matthews-air.lan>
2023-01-20 22:28:26 -0500

    please please work

--- postgres.js
+++ postgres.js
@@ -1,8 +1,8 @@
 const Client = require('pg').Client
 
 const dbConfig = {
-	// connectionString: 'postgresql://localhost:5432/pernfighter',
-    connectionString: 'postgres://hftywsabxjwdyl:e5369ccead4c24892e936543a1734c7931001fc46a164624e6fe39c3ff722dc1@ec2-52-21-136-176.compute-1.amazonaws.com:5432/d7d2co99pp7jmn',
+	connectionString: 'postgresql://localhost:5432/pernfighter',
+    // connectionString: 'postgres://hftywsabxjwdyl:e5369ccead4c24892e936543a1734c7931001fc46a164624e6fe39c3ff722dc1@ec2-52-21-136-176.compute-1.amazonaws.com:5432/d7d2co99pp7jmn',
 }
 
 if(process.env.DATABASE_URL){
